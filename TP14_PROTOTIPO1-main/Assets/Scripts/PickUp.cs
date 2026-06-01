67e9cca0223c37a67673fcb449c927dc8c380bed
ValentinaCampora <valencampora@gmail.com>
2026-06-01 10:39:18 -0300

    destruccion objetos

---
+++ TP14_PROTOTIPO1-main/Assets/Scripts/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Jugador entro al area");
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
