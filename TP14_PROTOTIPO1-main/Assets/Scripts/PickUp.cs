48c6e2523ff24bf380a87abd0442504137c33a96
ValentinaCampora <valencampora@gmail.com>
2026-06-01 13:21:50 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

--- TP14_PROTOTIPO1-main/Assets/Scripts/PickUp.cs
+++ TP14_PROTOTIPO1-main/Assets/Scripts/PickUp.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickUp : MonoBehaviour
 {
+
+    static int objetosRecolectados = 0;
+
+    public TextMeshProUGUI contadorTexto;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Jugador entro al area");
+            objetosRecolectados++;
+
+            contadorTexto.text = "Objetos recolectados: " + objetosRecolectados ;
+
             Destroy(gameObject);
         }
     }
